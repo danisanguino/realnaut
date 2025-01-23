46d758b9d0c1439dc9ea0d471de5aa86d8b10a5d
Daniel Sanguino <153718537+danisanguino@users.noreply.github.com>
2025-01-23 10:11:17 +0100

    build submenu with js

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -3,33 +3,56 @@ import HtmlWebpackPlugin from 'html-webpack-plugin';
 import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 
 export default {
-  entry: './src/js/index.js', 
-  output: {
-    filename: 'bundle.js', 
-    path: path.resolve('dist'), 
-  },
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.scss$/, 
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-      {
-        test: /\.js$/, 
-        exclude: /node_modules/,
-      },
+    entry: './frontend/src/js/scripts.js', 
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve('frontend', 'dist'), 
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+            },
+            {
+              test: /\.(png|jpe?g|gif|svg)$/i, 
+              type: 'asset/resource', 
+              generator: {
+                  filename: 'assets/[name].[hash][ext]', 
+              },
+          },
+          {
+            test: /\.html$/i,
+            loader: 'html-loader',
+            options: {
+                sources: {
+                    list: [
+                        {
+                            tag: 'img', // Procesar imágenes en etiquetas <img>
+                            attribute: 'src',
+                            type: 'src',
+                        },
+                    ],
+                },
+            },
+        },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './frontend/src/index.html', 
+        }),
+        new CleanWebpackPlugin(),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-    new CleanWebpackPlugin(),
-  ],
-  devServer: {
-    static: './dist',
-    port: 8080,
-    open: true,
-  },
+    devServer: {
+        static: './frontend/dist',
+        port: 8080,
+        open: true,
+    },
+    
 };
